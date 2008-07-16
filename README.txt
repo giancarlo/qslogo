cbe341b7089311339da2abd11719b86c49c6fc4f
giancarlo.bellido <giancarlo.bellido@8be7720a-1c52-0410-aeab-7b5676d02d99>
2008-07-16 04:58:09 +0000

    Added Proper Headers

--- README.txt
+++ README.txt
@@ -9,5 +9,16 @@ I.1 Modify logo.pro
 
 Before running qmake you need to set the BOOST variable with the location of your BOOST include files.
 
+I.2 Run Qmake
+
+I.3 Run make or nmake or other
+
+I.4 Enjoy
+
+II. Installation
+
+You will need qtcore4.dll and qtgui4.dll in the bin/(target) folder.
+
+
 
 	
--- logo/stack.cpp
+++ logo/stack.cpp
@@ -1,8 +1,21 @@
 /*
- *	Coaxial Software
- *	Copyright (c) 2007 - Giancarlo Bellido
+ *	
+ *	Copyright (c) 2008 - Giancarlo Bellido
  *
- *	stack.cpp
+ *	This file is part of qsLogo.
+ *
+ *  Foobar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  *  
  */
 
