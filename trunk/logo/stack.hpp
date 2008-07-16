a3b0d6fe554c03ab1daf26de8cd31cd6f536ea2a
giancarlo.bellido <giancarlo.bellido@8be7720a-1c52-0410-aeab-7b5676d02d99>
2008-07-16 04:58:09 +0000

    Added Proper Headers

--- trunk/logo/stack.hpp
+++ trunk/logo/stack.hpp
@@ -1,23 +1,23 @@
-/* 
-*      Copyright (C) 2006-2007 Coaxial IT Services
-*      http://coaxialhost.com
-*
-*  This Program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2, or (at your option)
-*  any later version.
-*   
-*  This Program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*  GNU General Public License for more details.
-*   
-*  You should have received a copy of the GNU General Public License
-*  along with GNU Make; see the file COPYING.  If not, write to
-*  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
-*  http://www.gnu.org/copyleft/gpl.html
-*
-*/
+/*
+ *	
+ *	Copyright (c) 2008 - Giancarlo Bellido
+ *
+ *	This file is part of qsLogo.
+ *
+ *  Foobar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ */
 
 #ifndef LOGO_STACK_HPP
 #define LOGO_STACK_HPP
