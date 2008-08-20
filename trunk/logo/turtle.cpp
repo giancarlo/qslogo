8221c635f84d9149b2ac12556fdd7eccfc5f4bda
giancarlo.bellido <giancarlo.bellido@8be7720a-1c52-0410-aeab-7b5676d02d99>
2008-08-20 04:12:04 +0000

    It finally draws lines

--- trunk/logo/turtle.cpp
+++ trunk/logo/turtle.cpp
@@ -8,11 +8,13 @@
 
 // TODO setOffset code sets the turtle offset in its center. 
 // This code might be better at another place.
-logo::turtle::turtle(QGraphicsScene* scene)
+logo::turtle::turtle(QGraphicsScene* scenes)
 {
 	// NOTE I believe this is deleted automatically? 
-	pixmap = scene->addPixmap(QPixmap(":/resources/turtle.png"));
+	pixmap = scenes->addPixmap(QPixmap(":/resources/turtle.png"));
 	pixmap->setOffset(-pixmap->pixmap().width()/2,-pixmap->pixmap().height()/2);
+	scene  = scenes;
+	pen.setWidth(1);
 }
 
 qreal logo::turtle::x()
@@ -27,7 +29,9 @@ qreal logo::turtle::y()
 
 void logo::turtle::forward(qreal count)
 {
+	QPointF last(pixmap->scenePos());
 	pixmap->translate(0, -count);
+	scene->addLine(QLineF(last, pixmap->scenePos()), pen);
 }
 
 void logo::turtle::backward(qreal count)
@@ -44,3 +48,4 @@ void logo::turtle::moveBy(qreal dx, qreal dy)
 {
 	pixmap->moveBy(dx, dy);
 }
+
